9c5e7db5c3cefb7ebfe1ac541af7c7f9fae6fbfa
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-03-21 12:51:08 -0600

    add initial methods

--- excel_parse.py
+++ excel_parse.py
@@ -14,5 +14,32 @@ def get_sheet_data(sheet):
     for n in range(sheet.ncols):
         print n
 
+def get_all_people(workbook):
+    pass
+
+def get_all_variables():
+    pass
+
+def get_funding_agencies():
+    pass
+
+def get_all_projects():
+    pass
+
+def get_dates():
+    pass
+
+def get_boundaries():
+    pass
+
+def get_ships():
+    pass
+
+def get_sea_areas():
+    pass
+
+def get_package_descriptions():
+    pass
+
 if __name__ == '__main__':
     main()
